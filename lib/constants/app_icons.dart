093780e04f819286ca42f3777d06018e5db4e9b0
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-22 12:30:58 +0530

    app_icons.dart: k add before all variable names to make all static, and constant directory add inside utils directory.

--- lib/constants/app_icons.dart
+++
@@ -1,9 +0,0 @@
-class AppIcons {
-  AppIcons._();
-
-  /// Onboarding icons
-  static const String onBoarding_icon1 = "assets/onboarding/onboarding_1.svg";
-  static const String onBoarding_icon2 = "assets/onboarding/onboarding_2.svg";
-  static const String onBoarding_icon3 = "assets/onboarding/onboarding_3.svg";
-  static const String onBoarding_icon4 = "assets/onboarding/onboarding_4.svg";
-}
---
+++ lib/utils/constants/app_icons.dart
@@ -0,0 +1,9 @@
+class AppIcons {
+  AppIcons._();
+
+  /// Onboarding icons
+  static const String kOnBoarding_icon1 = "assets/onboarding/onboarding_1.svg";
+  static const String kOnBoarding_icon2 = "assets/onboarding/onboarding_2.svg";
+  static const String kOnBoarding_icon3 = "assets/onboarding/onboarding_3.svg";
+  static const String kOnBoarding_icon4 = "assets/onboarding/onboarding_4.svg";
+}
