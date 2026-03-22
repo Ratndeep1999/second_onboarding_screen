fcade18165eb02b92ed7bed7a02575f261267abf
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-22 12:35:38 +0530

    onboarding_model.dart: properties and constructor add.

---
+++ lib/onboarding/model/onboarding_model.dart
@@ -0,0 +1,11 @@
+class OnboardingModel {
+  String title;
+  String desc;
+  String image;
+
+  OnboardingModel({
+    required this.title,
+    required this.desc,
+    required this.image,
+  });
+}
