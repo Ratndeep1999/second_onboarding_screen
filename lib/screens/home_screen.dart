b263688daddc979dff8fcefe81dd04756b16b529
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-23 10:14:36 +0530

    home_screen.dart: statelessWidget with Scaffold layout add.

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(title: Text("Home Screen")),
+    body: SafeArea(child: Column()),
+  );
+}
