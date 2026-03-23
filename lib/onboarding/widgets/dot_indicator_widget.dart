228b86852f29ecde0c90abed099a29726eb323ff
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-23 15:10:01 +0530

    dot_indicator_widget.dart: final int pageIndex; add, color: (index == pageIndex) ? Colors.black54 : Colors.black12 add, and Container change to AnimatedContainer().

--- lib/onboarding/widgets/dot_indicator_widget.dart
+++ lib/onboarding/widgets/dot_indicator_widget.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 class DotIndicatorWidget extends StatelessWidget {
-  const DotIndicatorWidget({super.key, required this.length});
+  const DotIndicatorWidget({
+    super.key,
+    required this.length,
+    required this.pageIndex,
+  });
 
   final int length;
+  final int pageIndex;
 
   @override
   Widget build(BuildContext context) {
@@ -11,14 +16,17 @@ class DotIndicatorWidget extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: List.generate(
         length,
-        (index) => Container(
+        (index) => AnimatedContainer(
           height: 10,
           width: 10,
           margin: EdgeInsets.symmetric(horizontal: 8),
           decoration: BoxDecoration(
-            color: Colors.black26,
+            /// Check Indication
+            color: (index == pageIndex) ? Colors.black54 : Colors.black12,
             shape: BoxShape.circle,
           ),
+          duration: Duration(milliseconds: 500),
+          curve: Curves.linear,
         ),
       ),
     );
