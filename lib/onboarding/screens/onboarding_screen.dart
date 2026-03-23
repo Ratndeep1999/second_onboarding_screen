47ffbbfc4c6ca85628e91c5450f579e0b9c1e3ce
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-23 11:23:21 +0530

    onboarding_screen.dart: DotIndicator() and GetStartedButton() widget refactor.

--- lib/onboarding/screens/onboarding_screen.dart
+++ lib/onboarding/screens/onboarding_screen.dart
@@ -16,68 +16,95 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       child: Column(
         children: [
           /// Skip Button
-          SkipButton(),
+          SkipButtonWidget(),
           SizedBox(height: 80),
 
           /// Onboarding Data
-          Column(
-            children: [
-              /// Image
-              SvgPicture.asset("assets/onboarding/onboarding_1.svg"),
-              SizedBox(height: 60),
-
-              /// Title
-              Text(
-                "Onboarding Title One",
-                style: TextStyle(
-                  fontSize: 28,
-                  letterSpacing: 2,
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              SizedBox(height: 30),
-
-              /// Description
-              Text(
-                "This is the description of onboarding title one.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  wordSpacing: 2,
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ],
-          ),
+          OnboardingDataWidget(),
           SizedBox(height: 60),
 
           /// Onboarding Page Indicator
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(
-              4,
-              (index) => Container(
-                height: 10,
-                width: 10,
-                margin: EdgeInsets.symmetric(horizontal: 8),
-                decoration: BoxDecoration(
-                  color: Colors.black26,
-                  shape: BoxShape.circle,
-                ),
-              ),
-            ),
-          ),
-          FilledButton(onPressed: () {}, child: Text("Get Started")),
+          DotIndicatorWidget(),
+          GetStartedButtonWidget(),
         ],
       ),
     ),
   );
 }
 
-class SkipButton extends StatelessWidget {
-  const SkipButton({super.key});
+class GetStartedButtonWidget extends StatelessWidget {
+  const GetStartedButtonWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(onPressed: () {}, child: Text("Get Started"));
+  }
+}
+
+class DotIndicatorWidget extends StatelessWidget {
+  const DotIndicatorWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(
+        4,
+        (index) => Container(
+          height: 10,
+          width: 10,
+          margin: EdgeInsets.symmetric(horizontal: 8),
+          decoration: BoxDecoration(
+            color: Colors.black26,
+            shape: BoxShape.circle,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class OnboardingDataWidget extends StatelessWidget {
+  const OnboardingDataWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        /// Image
+        SvgPicture.asset("assets/onboarding/onboarding_1.svg"),
+        SizedBox(height: 60),
+
+        /// Title
+        Text(
+          "Onboarding Title One",
+          style: TextStyle(
+            fontSize: 28,
+            letterSpacing: 2,
+            color: Colors.black38,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        SizedBox(height: 30),
+
+        /// Description
+        Text(
+          "This is the description of onboarding title one.",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            wordSpacing: 2,
+            color: Colors.black38,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SkipButtonWidget extends StatelessWidget {
+  const SkipButtonWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
