6412ef8b1b3907c266ee99a090b1f2e066438e1c
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-23 15:31:38 +0530

    onboarding_data_widget.dart: controller: pageCtrl, and onPageChanged: onPageChanged, add.

--- lib/onboarding/widgets/onboarding_data_widget.dart
+++ lib/onboarding/widgets/onboarding_data_widget.dart
@@ -1,56 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:seccond_onboarding_screen/onboarding/model/onboarding_model.dart';
 
 class OnboardingDataWidget extends StatelessWidget {
   const OnboardingDataWidget({
     super.key,
-    required this.length,
-    required this.imagePath,
-    required this.title,
-    required this.desc,
+    required this.onboardingData,
+    required this.pageCtrl,
+    this.onPageChanged,
   });
 
-  final int length;
-  final String imagePath;
-  final String title;
-  final String desc;
+  final List<OnboardingModel> onboardingData;
+  final PageController pageCtrl;
+  final Function(int)? onPageChanged;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: PageView.builder(
-        itemCount: length,
-        itemBuilder: (BuildContext context, int index) => Column(
-          children: [
-            /// Image
-            SvgPicture.asset(imagePath),
-            const SizedBox(height: 60),
+        controller: pageCtrl,
+        onPageChanged: onPageChanged,
+        itemCount: onboardingData.length,
+        itemBuilder: (BuildContext context, int index) {
+          /// Get Specific Items
+          final item = onboardingData[index];
+          return Column(
+            children: [
+              /// Image
+              SvgPicture.asset(item.image),
+              const SizedBox(height: 60),
 
-            /// Title
-            Text(
-              title,
-              style: TextStyle(
-                fontSize: 28,
-                letterSpacing: 2,
-                color: Colors.black38,
-                fontWeight: FontWeight.w600,
+              /// Title
+              Text(
+                item.title,
+                style: TextStyle(
+                  fontSize: 28,
+                  letterSpacing: 2,
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
-            const SizedBox(height: 30),
+              const SizedBox(height: 30),
 
-            /// Description
-            Text(
-              desc,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                wordSpacing: 2,
-                color: Colors.black38,
-                fontWeight: FontWeight.w400,
+              /// Description
+              Text(
+                item.desc,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  wordSpacing: 2,
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                ),
               ),
-            ),
-          ],
-        ),
+            ],
+          );
+        },
       ),
     );
   }
